9b991f994917664c23785195ead4098e8984b927
AlexFreik <freik.aa@phystech.edu>
2022-07-02 15:50:57 +0300

    husky test 3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "../styles/globals.css";
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
 
 export default MyApp;
-
